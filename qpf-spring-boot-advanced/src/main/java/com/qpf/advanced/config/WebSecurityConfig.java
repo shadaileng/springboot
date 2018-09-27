6eb605e13f4729f2cbe44f0bd01071462deb5a1b
shadaileng <qpf0510@qq.com>
2018-09-27 16:48:59 +0800

    rememberMe

--- qpf-spring-boot-advanced/src/main/java/com/qpf/advanced/config/WebSecurityConfig.java
+++ qpf-spring-boot-advanced/src/main/java/com/qpf/advanced/config/WebSecurityConfig.java
@@ -9,11 +9,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("qpf").password("123456").roles("VIP1", "VIP2", "VIP3")
+                .withUser("qpf@qq.com").password("1").roles("VIP1", "VIP2", "VIP3")
                 .and()
-                .withUser("sdl").password("123456").roles("VIP1", "VIP2")
+                .withUser("sdl@qq.com").password("1").roles("VIP1", "VIP2")
                 .and()
-                .withUser("shadaileng").password("123456").roles("VIP1", "VIP3");
+                .withUser("shadaileng@qq.com").password("1").roles("VIP1", "VIP3")
+                .and()
+                .withUser("qpf0510@qq.com").password("1").roles("VIP1", "VIP3");
     }
 
     @Override
@@ -23,7 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/level/level1/**").hasRole("VIP1")
                 .antMatchers("/level/level2/**").hasRole("VIP2")
                 .antMatchers("/level/level3/**").hasRole("VIP3");
-        http.formLogin();
-        http.logout().logoutSuccessUrl("/");;
+        http.formLogin().loginPage("/userlogin").usernameParameter("email").passwordParameter("password");
+        http.logout().logoutSuccessUrl("/");
+        http.rememberMe().rememberMeParameter("remember-me");
     }
 }
